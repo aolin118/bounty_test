a28db1e938757403b899123e3fcb7a236559ffff
Xane <xane@talentagroup.com>
2018-06-06 19:31:40 +0800

    Telegram Bot

--- resources/views/complete.blade.php
+++ resources/views/complete.blade.php
@@ -25,7 +25,7 @@
                 <div class="row mb-1 text-center">
                     <div class="col-12">
                         <h2 class="mb-4 text-white">Thank you for your participation. Please follow our Telegram for up to date information on the bounty program. </h2>
-                        <span class="text-mute">Your referral link is:</span>
+                        <span class="text-muted">Your referral link is:</span>
                         <h3><a href="{{ $link = route('airdrop-get') . "/r/" . $user->telegram_id }}">{{ $link }}</a></h3>
                     </div>
                     <div class="col-12 my-4" style="font-size: 14px">
