72fc5d4026af1e172863c403fd56016099ea9306
Xane <xane@talentagroup.com>
2018-08-29 12:18:27 +0800

    Add card

--- resources/views/instructions.blade.php
+++ resources/views/instructions.blade.php
@@ -30,7 +30,7 @@
                                 <p><b>Your Referral Link</b><br/><a href="{{ route('bounty-referral', $user->unique_link) }}" target="_blank">{{ route('bounty-referral', $user->unique_link) }}</a><br/><button type="button" class="btn btn-primary mt-2" id="copy-btn" data-clipboard-text="{{ route('bounty-referral', $user->unique_link) }}">Copy</button></p>
                                 <small class="text-danger">You will get 2 BCT for every referral that completes at least 1 task.</small>
 
-                                <p class="mt-4"><input type="checkbox" id="interestCheckBox" name="interestCheckBox" onclick="registerInterest();"><small class="ml-2">Yes, I am interested in getting early access to BCoin’s Prepaid Debit Card and have registered for an account at <a href="https://www.bcoin.sg/" target="_blank">BCoin.sg</a> Exchange.</small></p>
+                                <p class="mt-4 interest-container"><input type="checkbox" id="interestCheckBox" name="interestCheckBox" onclick="registerInterest();"><small class="ml-2">Yes, I am interested in getting early access to BCoin’s Prepaid Debit Card and have registered for an account at <a href="https://www.bcoin.sg/" target="_blank">BCoin.sg</a> Exchange.</small></p>
                                 <img src="{{ asset('images/diamond-card.png') }}" class="img-fluid diamond-card">
                             </div>
                         </div>
