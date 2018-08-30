48da7cb1c3a655b37dfb4041c82801a7d1fbd035
Xane <xane@talentagroup.com>
2018-08-30 19:45:07 +0800

    Additional bounty rewards

--- app/Http/Controllers/BountyController.php
+++ app/Http/Controllers/BountyController.php
@@ -78,7 +78,7 @@ class BountyController extends Controller
                                             ->orWhere('reddit_completed', 1)
                                             ->orWhere('youtube_completed', 1)
                                             ->orWhere('facebook_completed', 1)
-                                            ->orWhere('instagrm_completed', 1)
+                                            ->orWhere('instagram_completed', 1)
                                             ->orWhere('linkedin_completed', 1);
                                       })->count();
 
